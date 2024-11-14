1ed31dbcb94b72d3447e9a7c5fee2c7156aa4661
Medy Lavergne <mlavergn@fu-r10-p6.s19.be>
2024-11-14 15:09:15 +0100

    yy

--- get_next_line.c
+++ get_next_line.c
@@ -3,17 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
 /*   By: mlavergn <mlavergn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 19:05:50 by mlavergn          #+#    #+#             */
-/*   Updated: 2024/10/29 15:55:56 by mlavergn         ###   ########.fr       */
-=======
-/*   By: mlavergn <mlavergn@s19.be>                 +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/10 19:05:50 by mlavergn          #+#    #+#             */
-/*   Updated: 2024/08/11 08:39:15 by mlavergn         ###   ########.fr       */
->>>>>>> 20f667f612ffcb45b9788871985caa345563e07e
+/*   Updated: 2024/11/14 15:09:07 by mlavergn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +40,8 @@ void	fill_stash(int fd, char **stash)
 		if (bytes_read == -1)
 		{
 			free(buf);
-<<<<<<< HEAD
 			free(*stash);
 			*stash = NULL;
-=======
->>>>>>> 20f667f612ffcb45b9788871985caa345563e07e
 			return ;
 		}
 		buf[bytes_read] = '\0';
